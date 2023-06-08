1992f5eb5b9df31a6b6cfeeac6f631be02dd16e8
Csarabia2002 <sarabia200212@gmail.com>
2023-06-07 20:21:51 -0400

    CERTAMEN 2 FINALIZADO

--- colegio/comunicados/views.py
+++ colegio/comunicados/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Comunicado, Categoria
 # Create your views here.
 def home(request):
-    comunicados = Comunicado.objects.order_by('fecha_ultima_modificacion')
+    comunicados = Comunicado.objects.order_by('-fecha_ultima_modificacion')
 
     context ={'comunicados':comunicados}
 
